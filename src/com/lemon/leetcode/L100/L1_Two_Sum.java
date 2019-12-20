7dae6841e30b1df135dc3d64cacace2048d56624
lemonliu1993 <2305247091@qq.com>
2019-12-20 23:20:18 +0800

    L1.两数之和

---
+++ src/com/lemon/leetcode/L100/L1_Two_Sum.java
@@ -0,0 +1,29 @@
+package com.lemon.leetcode.L100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lemoon on 19/12/20 下午11:14
+ * note:求数组中两个数和为固定数字的方法：
+ *    一次遍历，使用map保存之前出现过的数的位置，到当前位置时找之前是否出现过该数。
+ */
+public class L1_Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length <= 1) {
+            return null;
+        }
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
