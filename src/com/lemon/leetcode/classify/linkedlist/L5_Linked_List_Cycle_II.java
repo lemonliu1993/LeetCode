4845f88d38f844d59f202e6ec2949b02e2063379
lemonliu1993 <2305247091@qq.com>
2020-01-21 16:13:24 +0800

    链表——环形链表2

---
+++ src/com/lemon/leetcode/classify/linkedlist/L5_Linked_List_Cycle_II.java
@@ -0,0 +1,63 @@
+package com.lemon.leetcode.classify.linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * leetcode140
+ * Created by lemoon on 20/1/11 上午10:21
+ */
+public class L5_Linked_List_Cycle_II {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    slow = head;
+                    while (fast != slow) {
+                        fast = fast.next;
+                        slow = slow.next;
+                    }
+                    return fast;
+                }
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public ListNode detectCycle1(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return head;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return null;
+
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
