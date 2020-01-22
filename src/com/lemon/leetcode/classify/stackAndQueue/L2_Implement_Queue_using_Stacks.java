69ea7a5c7b3cedb9c0fbb2fc4f3a20f0837d97b0
lemonliu1993 <2305247091@qq.com>
2020-01-22 11:43:35 +0800

    栈队列——用栈实现队列

---
+++ src/com/lemon/leetcode/classify/stackAndQueue/L2_Implement_Queue_using_Stacks.java
@@ -0,0 +1,52 @@
+package com.lemon.leetcode.classify.stackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * leetcode232
+ * Created by lemoon on 20/1/22 上午9:02
+ */
+public class L2_Implement_Queue_using_Stacks {
+
+    /** Initialize your data structure here. */
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+    public L2_Implement_Queue_using_Stacks() {
+
+    }
+
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(!stack2.isEmpty()){
+            return stack2.pop();
+        }else{
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            return stack2.pop();
+        }
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(!stack2.isEmpty()){
+            return stack2.peek();
+        }else{
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+            return stack2.peek();
+        }
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
