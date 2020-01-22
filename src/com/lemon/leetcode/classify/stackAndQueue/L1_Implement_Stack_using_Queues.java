5af28c5326144a2acc2b692a38efc43205233ff2
lemonliu1993 <2305247091@qq.com>
2020-01-22 11:43:11 +0800

    栈队列——用队列实现栈

---
+++ src/com/lemon/leetcode/classify/stackAndQueue/L1_Implement_Stack_using_Queues.java
@@ -0,0 +1,66 @@
+package com.lemon.leetcode.classify.stackAndQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * leetcode225
+ * Created by lemoon on 20/1/22 上午9:01
+ */
+public class L1_Implement_Stack_using_Queues {
+    /**
+     * Initialize your data structure here.
+     */
+    Queue<Integer> queue1 = new LinkedList<>();
+    Queue<Integer> queue2 = new LinkedList<>();
+
+    public L1_Implement_Stack_using_Queues() {
+
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        if (queue1.isEmpty()) {
+            queue1.add(x);
+            return;
+        } else {
+            while (!queue1.isEmpty()) {
+                queue2.add(queue1.poll());
+            }
+            queue1.add(x);
+            while (!queue2.isEmpty()) {
+                queue1.add(queue2.poll());
+            }
+        }
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue1.poll();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue1.peek();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        L1_Implement_Stack_using_Queues stack = new L1_Implement_Stack_using_Queues();
+        stack.push(1);
+        stack.pop();
+        System.out.println(stack.empty());
+    }
+}
