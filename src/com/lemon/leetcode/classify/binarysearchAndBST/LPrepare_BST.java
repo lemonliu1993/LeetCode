9159781aa8ad7d8314d91adfeb9b36e8b826f6b0
lemonliu1993 <2305247091@qq.com>
2020-01-31 21:52:35 +0800

    BST树插入

---
+++ src/com/lemon/leetcode/classify/binarysearchAndBST/LPrepare_BST.java
@@ -0,0 +1,73 @@
+package com.lemon.leetcode.classify.binarysearchAndBST;
+
+/**
+ * Created by lemoon on 20/1/30 下午10:54
+ */
+public class LPrepare_BST {
+
+
+    void insert(TreeNode root, TreeNode node) {
+        if (root == null) {
+            root = node;
+            return;
+        } else {
+            insertRecursive(root, node);
+        }
+    }
+
+    private void insertRecursive(TreeNode root, TreeNode node) {
+        if (root.val > node.val) {
+            if (root.left == null) {
+                root.left = node;
+            } else {
+                insertRecursive(root.left, node);
+            }
+        } else {
+            if (root.right == null) {
+                root.right = node;
+            } else {
+                insertRecursive(root.right, node);
+            }
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    void preOrder_print(TreeNode node,int level){
+        if(node==null){
+            return;
+        }
+        for(int i =0;i<level;i++){
+            System.out.print("---");
+        }
+        System.out.println(node.val);
+        preOrder_print(node.left,level+1);
+        preOrder_print(node.right,level+1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(8);
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(6);
+        TreeNode node4 = new TreeNode(10);
+        TreeNode node5 = new TreeNode(15);
+//        TreeNode node6 = new TreeNode(8);
+        LPrepare_BST lPrepare_bst = new LPrepare_BST();
+        lPrepare_bst.insert(node,node1);
+        lPrepare_bst.insert(node,node2);
+        lPrepare_bst.insert(node,node3);
+        lPrepare_bst.insert(node,node4);
+        lPrepare_bst.insert(node,node5);
+        lPrepare_bst.preOrder_print(node,0);
+
+    }
+}
