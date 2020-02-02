2d7edac3465c096b3dc999d707439991884d52ca
lemonliu1993 <2305247091@qq.com>
2020-02-02 17:12:09 +0800

    字符哈希——统计字符出现次数

---
+++ src/com/lemon/leetcode/classify/hashAndString/LPrepare_StringHash.java
@@ -0,0 +1,22 @@
+package com.lemon.leetcode.classify.hashAndString;
+
+/**
+ * Created by lemoon on 20/2/2 下午5:06
+ */
+public class LPrepare_StringHash {
+
+
+    public static void main(String[] args) {
+        int[] charMap = new int[128];
+        String s = "asfdzxvaxxt";
+        for (int i = 0; i < s.length(); i++) {
+            charMap[s.charAt(i)]++;
+        }
+
+        for (char i = 0; i < charMap.length; i++) {
+            if (charMap[i] != 0) {
+                System.out.println(i + "次数" + charMap[i]);
+            }
+        }
+    }
+}
